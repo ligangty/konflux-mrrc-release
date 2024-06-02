df75522432c9541cc5a276e568b88cfa6db65e1b
Gang Li <ligangty@users.noreply.github.com>
2024-06-02 15:08:17 +0800

    Add a mock go main to pass konflux build

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github/ligangty/konflux-mrrc-release
+
+go 1.21.10
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("This is a mock main function to let the Konlux build pass.")
+}
